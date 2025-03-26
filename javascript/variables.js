82554bfd09a3c3e9b0b4258957f30478bad08e84
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-26 11:42:49 +0000

    container config done

---
+++ javascript/variables.js
@@ -0,0 +1,14 @@
+const number=12345659
+let name ="Ajit kumar"
+var password = '1234356'
+name="sujit kumar"
+console.log(name)
+password="Ajit kumar"
+let colour
+console.log(password)
+console.table([number, password, name, colour ])
+/* 
+prefer not to use var in javascript instead of using var use let keyword in javascript
+*/
+// if we will declare only variables then in output it will show undefined message
+/* in "const " keyword once data is declared then it cannot be changed */
