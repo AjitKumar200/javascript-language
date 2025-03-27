b2d2c79c62b96c8178604228715b8c22d9ab586d
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-27 05:20:13 +0000

    container config done

---
+++ javascript/datatyopeconversion.js
@@ -0,0 +1,33 @@
+// let name = "Ajit kumar"
+// console.log(typeof(name));
+// valueInnumber=Number(name);
+// console.log(valueInnumber);
+// let n = null
+// console.log(typeof(n));
+// valueInnumber=Number(n);
+// console.log(valueInnumber);
+// let nam = undefined
+// console.log(typeof(nam));
+// valueInnumber=Number(nam);
+// console.log(valueInnumber);
+// let ame = true
+// console.log(typeof(ame));
+// valueInnumber=Number(ame);
+// console.log("\n");
+// console.log(valueInnumber);
+/*  "33" => it can be easily be converted in number but
+"33abc" it acn not be converted in number it will give NaN output
+and "true " gives an output as 1 and false as 0
+*/
+let a =1
+console.log(typeof(a));
+let d = Boolean(a)
+console.log(d);
+let b =0
+console.log(typeof(b));
+let c =""
+console.log(typeof(c));
+let e = Boolean(c);
+console.log(e);
+
+
