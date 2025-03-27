c5c3aafc20c46c937d9fd995f2a0f20f92b4db4d
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-27 09:30:05 +0000

    memory config done

--- javascript/datatypesandecma.js
+++ javascript/datatypesandecma.js
@@ -1,11 +1,22 @@
-a=("Ajit kumar");
-console.log(a);
-b=45;
-/* number =>2 to power 53
-(bigint) it is used for large number 
-string 
-boolean
-null
-*/
-console.log(typeof (a));
+// a=("Ajit kumar");
+// console.log(a);
+// b=45;
+// /* number =>2 to power 53
+// (bigint) it is used for large number 
+// string 
+// boolean
+// null
+// */
+// console.log(typeof (a));
+
+// primitive datatype and non primitive datatype(or reference datatype)
+// 7  types of primitive datatype 
+// 1 : string,Number,boolean, null, undefined,symbol, BigInt
+
+// reference types : array,objects,function
+//  const id=Symbol('123')
+//  console.log(id)
+//  const AnotherId=symbol('123')
+//  console.log(AnotherId);
+//  console.log(id===AnotherId);
 
---
+++ javascript/memory.js
@@ -0,0 +1,20 @@
+// primitive datatype use stack memory that directly store the value in memory
+//non-primitive datatype uses heap memory that store the value by references in memory
+// let myname = "Ajit kumar"
+// let Anothername= myname;
+// Anothername="jamuna"
+// console.log(myname);
+// console.log(Anothername)
+// console.log(myname===Anothername);
+
+
+let myinfo={
+    name:"Ajit",
+    age:21
+
+}
+let mybio = myinfo
+ mybio.name="sujeet"
+
+console.log(myinfo.name)
+console.log(mybio.name)
