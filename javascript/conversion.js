300dba238012c53af522aa8dd04770b44b264645
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-27 07:31:56 +0000

    conversion config done

---
+++ javascript/conversion.js
@@ -0,0 +1,26 @@
+// let number = 33;
+// console.log(number)
+// let IsString= String(number)
+// console.log(IsString);
+// console.log(typeof(IsString));
+
+// let NegValue = -number;
+// console.log(NegValue)
+
+// let str1="Ajit kumar";
+// let str2="Sujit kumar";
+// let str3 = str1 +""+str2;
+// console.log(str3);
+// console.log(1+2+"3")// it will give 33 as output
+// console.log("1"+2+3);// it will give 123 as an output
+/* conclusion is that if we add something and if first omnre is string then it will give directly string as an output
+and if we entered first numeric input and then string then the output is addition of two numbers and string too*/
+// console.log(true)
+// console.log(+true)
+// console.log(+false)
+
+let num = 23;
+// num++
+console.log(num);
+++num
+console.log(num)
