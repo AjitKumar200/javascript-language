ae7c91cb980ae245d214d136cee57406277be585
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-27 10:23:55 +0000

    string config done

---
+++ javascript/string.js
@@ -0,0 +1,17 @@
+// let name = "Ajit kumar"
+// let myage = 21
+// console.log(name + myage)
+// console.log(`my name is ${name.toUpperCase()} and my age is ${myage}`);
+
+// how to declare string
+
+const name = new String("Ajit kumar");
+// console.log(name);
+// console.log(name[2]);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf('t'));
+const NewString= name.substring(0,4);
+console.log(NewString);
+const Anotherstring = name.slice(-8,4);
+console.log(Anotherstring)
