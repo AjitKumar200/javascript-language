e5b1fd76de46b0ec049d1c1973864373e684e331
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-27 08:05:35 +0000

    comparison config done

---
+++ javascript/comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(21<45);
+// console.log(null>0)
+// console.log(null<0);
+// console.log(null>=0);
+/* if we compared null to zero in less and greatorand ==it will gives 
+false as an output but if we compared to <= to number then 
+it will give true as an output because comparisions converts null into zero
+that's why it gives as true output */
+//(===)it not only compare the inputs and also check the datatypes of the input
+
+console.log("2" === 2);
+
