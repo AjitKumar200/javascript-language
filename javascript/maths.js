985737098b7f68e07a6c70e3246d367859f98146
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-28 07:50:25 +0000

    maths config done

---
+++ javascript/maths.js
@@ -0,0 +1,13 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+// str=(balance.toString());
+// console.log(str)
+// console.log(str.length)
+// console.log(typeof(str));
+// console.log(typeof(balance));
+const Number=357.346431
+// console.log(Number.toFixed(2));
+console.log(Number.toPrecision(3))
+
