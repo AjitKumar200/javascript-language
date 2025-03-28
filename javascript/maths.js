0bc9d78fc759415586325d14c259a34edf09cb92
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-28 08:51:52 +0000

    maths config done

--- javascript/maths.js
+++ javascript/maths.js
@@ -7,7 +7,15 @@
 // console.log(str.length)
 // console.log(typeof(str));
 // console.log(typeof(balance));
-const Number=357.346431
-// console.log(Number.toFixed(2));
-console.log(Number.toPrecision(3))
-
+// const Number=357.346431
+// // console.log(Number.toFixed(2));
+// console.log(Number.toPrecision(3))
+// const number = 100000000;
+// console.log(number);
+// console.log(number.toLocaleString());
+// console.log(number.toLocaleString('en-IN'));
+// console.log(Math.abs(-10))
+// console.log(Math.ceil(5.1))
+// console.log(Math.floor(5.4))
+// console.log(Math.round(5.456))
+// console.log(Math.sqrt(25))
