f0cd86b2ecbf07a48f5591403db6dc214418a6da
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-28 06:43:14 +0000

    all config done

---
+++ javascript/practice.js
@@ -0,0 +1,10 @@
+// console.log("cat".charAt(1));
+// let a = "Ajit kumar"
+// console.log(a.length)
+// console.log(a.__proto__);
+
+// console.log(a.__proto__ === String.prototype); // true
+// console.log(a.indexOf('k'))
+// console.log(a.substring(2))
+const a = ("     Ajit kumar     ");
+console.log(a.trim)
--- javascript/string.js
+++ javascript/string.js
@@ -5,13 +5,23 @@
 
 // how to declare string
 
-const name = new String("Ajit kumar");
-// console.log(name);
-// console.log(name[2]);
-// console.log(name.toUpperCase());
-// console.log(name.charAt(2));
-// console.log(name.indexOf('t'));
-const NewString= name.substring(0,4);
-console.log(NewString);
-const Anotherstring = name.slice(-8,4);
-console.log(Anotherstring)
+// const name = new String("Ajit kumar");
+// // console.log(name);
+// // console.log(name[2]);
+// // console.log(name.toUpperCase());
+// // console.log(name.charAt(2));
+// // console.log(name.indexOf('t'));
+// const NewString= name.substring(0,4);
+// console.log(NewString);
+// const Anotherstring = name.slice(-8,4);
+// console.log(Anotherstring)
+// const a = ("     Ajit kumar   ");
+// console.log(a)
+// console.log(a.trim())
+// console.log(a.trimStart())
+// console.log(a.trimEnd())
+
+const url = "https://hitesh.com/Ajit@06kumar"
+console.log(url.replace("@06","-"));
+const newUrl=url.split("//");
+console.log(newUrl);
