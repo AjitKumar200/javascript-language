4e2f7bf0397b347cf7a7fc815ec0e9edcf98cfa6
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-29 07:04:48 +0000

    change in date done

--- javascript/date.js
+++ javascript/date.js
@@ -3,3 +3,10 @@ let mydate = new Date();
 // console.log(mydate.toString())
 // console.log(mydate.toDateString());
 // console.log(mydate.toLocaleDateString())
+// let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(new Date);
+
+console.log(mydate .getMonth())
+console.log(mydate .getYear())
+console.log(mydate .getDay())
