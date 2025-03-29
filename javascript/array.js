88ba4ea0b97bbcba6c158d0eadbb7d47d514efc0
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-03-29 07:57:07 +0000

    array config done

---
+++ javascript/array.js
@@ -0,0 +1,13 @@
+// let myarr = [1,2,3,4,5]
+// console.log(myarr)
+// // myarr.push(6)
+// // nsole.log(myarr);
+// // myarr.pop()
+// // console.log(myarr);
+// myarr.unshift(5)
+// console.log(myarr);
+// myarr.shift()
+// console.log(myarr)
+let myarr = [1,3,2,4,5,6];
+let myarr1 = [6,7,8,9,0];
+let myarr2 = 
