0efe2cf3fec8d7519b6eb1fa5214e1defc015822
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-01 07:54:49 +0000

    scope config done

---
+++ javascript/scope.js
@@ -0,0 +1,46 @@
+//  let a = 300;
+ 
+ 
+ 
+//  if  (true){
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+//     console.log("inner is ", a);
+
+// }
+// console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+
+// function one(){
+//     const name = "ajit kumar"
+//     // console.log(website)
+//     function two(){
+//         const website = "chrome"
+//         console.log(name)
+//     }
+//     // two()
+
+// }
+// one(two)
+if (true){
+    const username = "Ajit kumar"
+    if(username==="Ajit kumar"){
+        const website = " youtube"
+        // console.log(username + website)
+
+    }
+}
+// ++++++++++++++++++++++++++++++++++++ interesting+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(addone(5))
+function addone(num){
+    return num+1;
+
+}
+addtwo(5)
+const c= function addtwo(num2){
+    return num2+2;
+}
+// console.log(addtwo()5)
