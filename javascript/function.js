c1272a7b9aa7d4b222eba64f3bce434c640085bd
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-01 06:30:57 +0000

    function config done

---
+++ javascript/function.js
@@ -0,0 +1,32 @@
+// // function calculate(x,y){
+// //     return x+y;
+    
+// // }
+// // console.log(calculate(5,3));
+
+// // function cart(...items){
+// //     return items;
+// // }
+// // console.log(cart(100,200,300,455,500))
+
+// const user = {
+//     username : "Ajit",
+//     price : 2500000
+// }
+// function handleobject(anyobject){
+//     console.log(`My name is ${anyobject.username} and price is ${anyobject.price}`)
+
+// }
+// // handleobject(user);
+// handleobject({
+//     username : "Ajit kumar",
+//     price : 4500000
+// });
+
+// const myarr = [1,2,3,4,5];
+
+// function returnvalue(getarray){
+//     return getarray[1]
+
+// }
+// console.log(returnvalue(myarr))
