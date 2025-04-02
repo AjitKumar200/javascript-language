e56d20fec7b0f10b796a41c44a610c16314a619f
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-02 10:38:10 +0000

    config done

---
+++ javascript/IIFE.js
@@ -0,0 +1,11 @@
+// // Immediately  Invoked Function Expressions( IIFE)
+// (function iife(){
+//     console.log(`Ajit kumar`);
+
+// })();
+// // iife ()
+
+// ( () => {
+//      console.log(`Ajit kumar twice`);
+    
+// }) ()
