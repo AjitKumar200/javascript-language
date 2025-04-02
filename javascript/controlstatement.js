136c50fae18a1d846d1635313116ca6cb9c149f0
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-02 11:21:34 +0000

    control statemnt config done

---
+++ javascript/controlstatement.js
@@ -0,0 +1,29 @@
+//   if statement 
+
+// if (2 == "2"){
+//     console.log(`true`);
+    
+// }
+// if (2 === "2"){
+//     console.log(`true`);
+    
+// }
+// else {
+//     console.log("false")
+// }
+
+// const score = 2000;
+// if (score > 1000){
+//     let power = "enough to fly";
+//     console.log(`flight has speed  ${power}`)
+// }
+// console.log(power);  the main problem to use (var) keyword is that 
+// it can be accesed globally in th code it will create confusion
+// better use let or const keyword
+
+//  short hand trick to CONTROL STATEMENT
+const balance = 1000;
+if (balance>500){
+    console.log(`less than 1000`);
+
+} 
