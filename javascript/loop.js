310ce16498ffb230741d8cd194f958fa9b12607e
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-03 11:11:14 +0000

    loops completed

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,5 +1,60 @@
 //  for loop  
-for (let index = 0; index <= 10; index++){
-    // const element = index;
-    console.log(index);
-}
+// for (let index = 0; index <= 10; index++){
+//     // const element = index;
+//     // console.log(index);
+
+//     if (index == 5){
+//         console.log("5 is the best number")
+//     }
+//     console.log(index);
+// }
+
+// for (let i = 1; i <=10; i++){
+//     for (let j = 1; j <= 10; j++){
+//         console.log(i + "*" + j  + "=" + i*j);
+//     }
+// }
+
+//   let arr  = ["Ajit kumar","Sujit kumar", "dhawla devi", "Devendra kumar"]
+//   for (let i = 0; i <arr.length; i++){
+//     const element = arr[i];
+//     // const element = i;
+//     console.log(element)
+//   }
+
+// for (let i = 0; i <= 20; i++){
+//     if( i == 5){
+//         console.log(" 5 is Detected");
+//         continue;
+//     }
+//     console.log(i);
+// }
+// for (let i = 0; i <= 20; i++){
+//     if( i == 5){
+//         console.log(" 5 is Detected");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// +++++++++++++++++++ do while loop ++++++++++++++++++++++++++
+
+// let i = 0;
+// while( i <= 10){
+//     console.log(i);
+//     i += 2
+// }
+
+// let arr = ["Ajit kumar","Sujit kumar","Dhawla devi"]
+// let arrl = 0;
+// while(arrl < arr.length){
+//     console.log(`Value of the array is ${arr[arrl]}`)
+//         arrl += 1
+    
+// }
+
+let score =11;
+do{
+    console.log(`score is ${score}`);
+    score ++;
+} while( score <= 10);
