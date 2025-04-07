57c963f68df4188d9ccbc65668a4218ed594904c
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-07 04:11:38 +0000

    arrayloops done

--- javascript/arrayloops.js
+++ javascript/arrayloops.js
@@ -16,14 +16,62 @@
 
 // +++++++++ Map datatype ++++++++++++
 
-const datatype =  new Map();
-datatype.set('IN',"India");
-datatype.set('Us',"United states");
-datatype.set('En',"England");
-// console.log(datatype)
-// for ( const key of datatype){
-//     console.log( key )
+// const datatype =  new Map();
+// datatype.set('IN',"India");
+// datatype.set('Us',"United states");
+// datatype.set('En',"England");
+// // console.log(datatype)
+// // for ( const key of datatype){
+// //     console.log( key )
+// // }
+// for ( const [key, value] of datatype){
+//         // console.log( key , ":-" , value )
 // }
-for ( const [key, value] of datatype){
-        console.log( key , ":-" , value )
-}
+
+// // ++++++++++++++++++++++ for loop ++++++++++++++++++
+// const myObject = {
+//         js : "javascript",
+//         cpp : "c++",
+//         c:    "c",
+//         rb : "ruby",
+//         py : "python",
+//         java : "java"
+
+// }
+// for (const object in  myObject){
+// //      console.log(object);
+
+// //  console.log(`${object} is for ${myObject[object]}`);
+// }
+
+// // ++++++++++++++++ special cases +++++++++++++++++
+
+// const languages = ["Ajit ", "java","python","c++","c"]
+// // languages.forEach(function(name) {
+// //   console.log(name);
+// // })
+
+// languages.forEach( (item) => {
+// //  console.log(item);
+// })
+
+// ++++++++++ array k andar object +++++++++++
+
+const mycoding = [
+        {
+                languageName : "javascript",
+                languageFileName : "js"
+        },
+        {
+                languageName : "java",
+                languageFileName : "java"
+        }, 
+        {
+                 languageName : "python",
+                 languageFileName : "py"
+        }
+]
+
+mycoding.forEach( (items) =>{
+        console.log(items.languageName);
+})
