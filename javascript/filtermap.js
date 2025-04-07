7f798de117f36e41530337e342519c4b649e9997
Ajit Kumar <kumarajit.cse06@gmail.com>
2025-04-07 12:04:26 +0000

    filter map config done

---
+++ javascript/filtermap.js
@@ -0,0 +1,32 @@
+// const MyNums = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = [];
+// MyNums.forEach ((num) => {
+//     if (num > 4){
+// newNums.push(num);
+//     }
+// })
+// // console.log(newNums);
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // const newNums = myNums.filter( (num) => {
+// //     return num > 4
+// // } )
+
+// let myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+// // const newNumbers = myNumbers.map((array) => array + 10);
+// const newNumbers = myNumbers.map((array) => {
+//     return array + 10;
+// })
+//  console.log(newNumbers);
+
+
+// ++++++++++++++++++chaining method ++++++++++++
+const items = [1,2,3];
+// const mytotal = items.reduce(function(acc,curval){
+//     console.log(`acc : ${acc} and curval: ${curval}`)
+//     return acc + curval
+// });
+
+
+console.log(mytotal);
